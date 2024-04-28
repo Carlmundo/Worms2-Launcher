d8b8e3965f0c4aa9f8fb11bd997e7155349d9cb9
Carl Norton <Carlmundo@users.noreply.github.com>
2024-04-28 11:52:38 +0100

    Update MainWindow.xaml.cs

--- Worms2/MainWindow.xaml.cs
+++ Worms2/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using System.Threading;
 using System.Windows;
 using System.Windows.Input;
